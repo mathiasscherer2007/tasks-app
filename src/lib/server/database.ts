6512e6c4a60e2798e337d2fd0d98d5da970e2f03
Mathias Scherer <mathischerer@gmail.com>
2026-03-31 07:46:06 -0300

    Create mock database

---
+++ src/lib/server/database.ts
@@ -0,0 +1,47 @@
+import type { Todo } from "./types/todo";
+
+// Simulates a database
+const db = new Map();
+
+export function getTodos(userid: string) {
+	if (!db.get(userid)) { // No primeiro get de um usuário, adiciona uma task "Welcome!"
+		db.set(userid, [
+			{
+				id: crypto.randomUUID(),
+				description: 'Welcome!',
+				done: false
+			}
+		]);
+	}
+
+	return db.get(userid);
+}
+
+export function createTodo(userid: string, description: string) {
+    if (description === '') {
+        throw new Error('todos must have a description');
+    }
+
+    const todos = db.get(userid);
+
+    if (todos.find((todo: Todo) => todo.description === description)) {
+        throw new Error('todos must be unique');
+    }
+
+    todos.push({
+        id: crypto.randomUUID(),
+        description: description,
+        done: false
+    })
+}
+
+export function deleteTodo(userid: string, todoid: string) {
+    const todos = db.get(userid);
+    const index = todos.findIndex((todo: Todo) => todo.id = todoid)
+
+    if (index !== -1) {
+        todos.splice(index, 1);
+    }
+}
+
+// TODO: edit tasks.
