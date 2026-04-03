5b67b464c0bb6a790d5a733ec617595ab1deeddf
Mathias Scherer <mathischerer@gmail.com>
2026-04-03 15:22:51 -0300

    Add styling to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,43 +3,36 @@
 	import TodoListItem from '$lib/components/TodoListItem.svelte';
 
 	let { data, form } = $props();
-
-	
 </script>
 
 {#if form?.error}
 	{alert(form.error)}
 {/if}
 
-<div class="centered">
-	<h1>TODOS</h1>
-	<form method="POST" action="?/create" use:enhance>
-		<input
-			name="description"
-			value={form?.description ?? ''}
-			autocomplete="off"
-			type="text"
-			placeholder="Add a todo"
-			required
-		/>
-		<button>Add</button>
-	</form>
+<div class="w-screen h-screen p-10">
+	<h1 class="w-fit text-3xl font-bold mb-5 text-transparent bg-clip-text bg-linear-to-l from-purple-500 to-indigo-600">TODOS</h1>
+	<div class="w-1/2 flex flex-col">
+		<form class="flex-1 flex gap-3" method="POST" action="?/create" use:enhance>
+			<input
+				name="description"
+				value={form?.description ?? ''}
+				autocomplete="off"
+				type="text"
+				placeholder="Add a todo"
+				required
+				class="p-3 flex-1 border rounded focus:outline-2 focus:outline-indigo-500"
+			/>
+			<button class="rounded px-4 bg-indigo-500 text-white cursor-pointer hover:bg-indigo-600">Add</button>
+		</form>
 
-	<ul>
-		{#if data.todos.length > 0}
-			{#each data.todos as todo (todo.id)}
-				<TodoListItem description={todo.description} id={todo.id} createdAt={todo.createdAt} />
-			{/each}
-		{:else}
-			<li>no data do display</li>
-		{/if}
-	</ul>
+		<ul class="w-full my-5 flex flex-col gap-2 p-2">
+			{#if data.todos.length > 0}
+				{#each data.todos as todo (todo.id)}
+					<TodoListItem description={todo.description} id={todo.id} createdAt={todo.createdAt} />
+				{/each}
+			{:else}
+				<li class="text-gray-500 italic">no data do display :(</li>
+			{/if}
+		</ul>
+	</div>
 </div>
-
-<style>
-	.centered {
-		height: 100vh;
-		width: fit-content;
-		margin: 0 auto;
-	}
-</style>
