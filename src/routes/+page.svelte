fc27f654975a9fd82f47bdcd0dd728b4e46cfb32
Mathias Scherer <mathischerer@gmail.com>
2026-04-03 15:28:00 -0300

    Dark mode style to main page (because reasons)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,8 @@
 	{alert(form.error)}
 {/if}
 
-<div class="w-screen h-screen p-10">
-	<h1 class="w-fit text-3xl font-bold mb-5 text-transparent bg-clip-text bg-linear-to-l from-purple-500 to-indigo-600">TODOS</h1>
+<div class="dark:bg-slate-800 w-screen h-screen p-10">
+	<h1 class="dark:text-white w-fit text-3xl font-bold mb-5">TODOS</h1>
 	<div class="w-1/2 flex flex-col">
 		<form class="flex-1 flex gap-3" method="POST" action="?/create" use:enhance>
 			<input
@@ -20,12 +20,12 @@
 				type="text"
 				placeholder="Add a todo"
 				required
-				class="p-3 flex-1 border rounded focus:outline-2 focus:outline-indigo-500"
+				class="dark:text-white p-3 flex-1 border rounded focus:outline-2 focus:outline-indigo-500"
 			/>
-			<button class="rounded px-4 bg-indigo-500 text-white cursor-pointer hover:bg-indigo-600">Add</button>
+			<button class="rounded px-4 bg-indigo-600 text-white cursor-pointer hover:bg-indigo-500 transition">Add</button>
 		</form>
 
-		<ul class="w-full my-5 flex flex-col gap-2 p-2">
+		<ul class="dark:text-white w-full my-5 flex flex-col gap-2 p-2">
 			{#if data.todos.length > 0}
 				{#each data.todos as todo (todo.id)}
 					<TodoListItem description={todo.description} id={todo.id} createdAt={todo.createdAt} />
